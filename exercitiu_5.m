7f595cf1aa09ea0e7c1b2cceabf6dea474b743a4
EfiaProdan <prodanmariaefia@yahoo.com>
2016-11-06 16:55:24 +0200

    Update exercitiu_5.m

--- exercitiu_5.m
+++ exercitiu_5.m
@@ -1,9 +1,9 @@
-f=0.25
-t=0:0.002:10;
+f=0.25        %f pentru perioada 4s
+t=0:0.002:10; %rezolutie 0.002
 A=1.5;
-y=abs(A*sin(2*pi*0.25*t));
+y=abs(A*sin(2*pi*0.25*t));             %abs imi returneaza valoarea absoluta a fiecarui element multime
 plot(t,y), grid;
-axis([0,10,-1,2])
+axis([0,10,-1,2])                      %limitelepe grafic
 title('Semnal sinusoidal dublu alternanta de rezolutie 0.002')
 xlabel('Timp [s]')
 ylabel('Amplitudine [V]')
