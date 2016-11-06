dd5a48e9372e9dc6364fb83d4b8a14339817f5db
EfiaProdan <prodanmariaefia@yahoo.com>
2016-11-06 14:47:22 +0200

    Update exercitiu_1_1.m

--- exercitiu_1_1.m
+++ exercitiu_1_1.m
@@ -1,7 +1,8 @@
-f=0.5;                           
+f=0.5;                                    %acelasi lucru l-am facut precum in exercitiu_1, doar ca am schimbat rezolutia la 0.02         
 t=0:0.02:10;
+A=0.75;
 y=square(2*pi*f*t,25);            
-plot(t,y+0.5);                  
+plot(t,y-0.5);                  
 axis([0 10 -2 2]),grid;              
 title('Semnal dreptunghiular rezolutie 20ms') 
 xlabel('timp')
