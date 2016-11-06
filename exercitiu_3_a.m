99cd71b41ad868bf41df6a501b5994d4183a3b24
EfiaProdan <prodanmariaefia@yahoo.com>
2016-11-06 15:01:30 +0200

    Update exercitiu_3_a.m

--- exercitiu_3_a.m
+++ exercitiu_3_a.m
@@ -1,4 +1,4 @@
-f=2;
+f=2;                       %pentru fiecare nivel am 0.25s, avand 2 nivele rezulta un T de 0.5s si frecventa 2
 t=0:0.002:2
-y=square(2*pi*f*t);
+y=square(2*pi*f*t);          
 plot(t,y), grid;
