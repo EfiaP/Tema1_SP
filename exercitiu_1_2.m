a2704ef5c2266081f92c801cdc2fcf2cf0408adb
EfiaProdan <prodanmariaefia@yahoo.com>
2016-11-06 14:47:45 +0200

    Update exercitiu_1_2.m

--- exercitiu_1_2.m
+++ exercitiu_1_2.m
@@ -1,4 +1,4 @@
-f=0.5;                           
+f=0.5;                                        %acelasi lucru l-am facut precum in exercitiu_1, doar ca am schimbat rezolutia la 0.2  
 t=0:0.2:10;
 y=square(2*pi*f*t,25);            
 plot(t,y+0.5);                  
