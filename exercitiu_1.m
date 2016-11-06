9e5c4cdcbf0f6628879a696028c507d1542d1654
EfiaProdan <prodanmariaefia@yahoo.com>
2016-11-06 12:24:50 +0200

    Add files via upload

---
+++ exercitiu_1.m
@@ -0,0 +1,15 @@
+F0=0.5;                           
+t=0:0.002:10;
+y=square(2*pi*F0*t,25);            
+plot(t,y+0.5);                  
+axis([0 10 -2 2]),grid;              
+title('Semnal dreptunghiular rezolutie 2ms') 
+xlabel('timp')
+ylabel('amplitudine')
+
+
+
+
+
+
+
