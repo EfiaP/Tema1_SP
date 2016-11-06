4d56fe55890b674e83dd80640d4078f80e8de1e3
EfiaProdan <prodanmariaefia@yahoo.com>
2016-11-06 16:56:14 +0200

    Update exercitiu_5_2.m

--- exercitiu_5_2.m
+++ exercitiu_5_2.m
@@ -1,9 +1,9 @@
 f=0.25
-t=0:0.2:10;
+t=0:0.2:10;     %rezolutie 0.2s
 A=1.5;
 y=abs(A*sin(2*pi*0.25*t));
 plot(t,y), grid;
 axis([0,10,-1,2])
-title('Semnal sinusoidal dublu alternanta de rezolutie 0.002')
+title('Semnal sinusoidal dublu alternanta de rezolutie 0.2')
 xlabel('Timp [s]')
 ylabel('Amplitudine [V]')
