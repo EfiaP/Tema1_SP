4bcc451647674976c9b98e3c8087275fda72bbe9
EfiaProdan <prodanmariaefia@yahoo.com>
2016-11-06 15:47:39 +0200

    Add files via upload

--- exercitiu_4_1.m
+++ exercitiu_4_1.m
@@ -1,6 +1,21 @@
-f=0.333
-t=0:0.002:10;
+f=0.33;
+t=0:0.002:20;
 A=0.8
-y=A*sin(2*pi*0.333*t);
-plot(t,y), grid;
-axis([0,10,-4,4])
+y=A*sin(2*pi*f*t);
+%y_mono=zeros(size(y));
+
+plot(y);
+grid
+for i=1:1:lenght(y);
+    if y(i)>0
+        y(i)=0;
+    end
+         
+end
+plot(y);
+axis([0 10 -2 2])
+grid
+
+
+
+
