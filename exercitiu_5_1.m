0431e079537ce35a063fe105e8b92083771c5bcb
EfiaProdan <prodanmariaefia@yahoo.com>
2016-11-06 16:56:38 +0200

    Update exercitiu_5_1.m

--- exercitiu_5_1.m
+++ exercitiu_5_1.m
@@ -4,6 +4,6 @@ A=1.5;
 y=abs(A*sin(2*pi*0.25*t));
 plot(t,y), grid;
 axis([0,10,-1,2])
-title('Semnal sinusoidal dublu alternanta de rezolutie 0.002')
+title('Semnal sinusoidal dublu alternanta de rezolutie 0.02')
 xlabel('Timp [s]')
 ylabel('Amplitudine [V]')
