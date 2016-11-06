578cc9244ada76be30dd3e4195e87c042c380c52
EfiaProdan <prodanmariaefia@yahoo.com>
2016-11-06 14:52:57 +0200

    Update exercitiu_2.m

--- exercitiu_2.m
+++ exercitiu_2.m
@@ -1,8 +1,10 @@
-f=0.2
-t=0:0.2:20;
+f=0.2                                           %frecventa 0.2 pentru o perioada de 5s
+t=0:0.002:20;
 A=1.5;
-y=A*sawtooth(2*pi*0.2*t,0.2)
-plot(t,y-0.5), grid;
+y=A*sawtooth(2*pi*0.2*t,0.2)                    %functia sawtooth genereaza un semnal triunghiular 
+                                                %0.2 e parametrul scalar care determina punctul intre 0 si 2pi unde se produce maximul
+                                                %am avut nevoie de acest parametru pentru a avea o panta de crestere de 1V/s
+plot(t,y-0.5), grid;                            %scad 0.5 pentru a avea maxim de 1 si minim de -2 (A=1.5)
 axis([0,20,-3,3])
 title('Semnal triunghiular')
 xlabel('Timp [s]')
