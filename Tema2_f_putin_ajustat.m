f67391dce4e672010e828c825774d198ca00635a
EfiaProdan <prodanmariaefia@yahoo.com>
2017-01-08 21:18:03 +0200

    Add files via upload

---
+++ Tema2_f_putin_ajustat.m
@@ -0,0 +1,54 @@
+
+clear;                             
+clf;                              
+
+N = 50; 
+P=40;                              
+wo =pi; 
+                          
+c0 = 0;                           
+t = -4:0.01:4;                     % rezolutie
+
+figure(1)                          
+
+% SF exponentiala
+
+x = c0*ones(size(t));                     % initializ xce cu c0
+
+for n = -N+1:2:N-1,                    
+  cn = 2/(j*n*wo);                        % Coeficientii seriei fourieri
+  x = x + real(cn*exp(j*n*wo*t)); %SFE
+end
+
+
+plot([ -4 -3 -3 -2 -2 -1 -1  0 0 1  1  2 2 3 3 4],...    % plot 
+     [1 1 -1 -1 1 1 -1 -1 1 1 -1 -1 1 1 -1 -1], ':');
+hold;
+plot(t,x);			    % plot SFE
+xlabel('t (secunde)'); ylabel('x(t)');
+title('Semnalul initial si semnalul reconstruit');
+hold;
+
+% Spectrul de amplitudine a SFE
+
+figure(2)                          % figura 2
+
+
+stem(0,c0);                       
+                                   %folosim stem pentru discret
+
+hold;
+for n = -N+1:2:N-1,                    
+  cn = 2/(j*n*wo);                 % Coeficientii
+  stem(n*wo,abs(cn))               
+end
+for n = -N:2:N,                
+  cn = 0;                         
+  stem(n*wo,abs(cn));              
+end
+
+xlabel('w (rad/s)')
+ylabel('c')
+title('Spectrul de amplitudine'); 
+grid;
+hold;
