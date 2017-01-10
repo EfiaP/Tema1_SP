d2aac5bf205f2cdc2b2004518fd320d3993cd52f
EfiaProdan <prodanmariaefia@yahoo.com>
2017-01-10 16:10:46 +0200

    Update Tema2_f_putin_ajustat.m

--- Tema2_f_putin_ajustat.m
+++ Tema2_f_putin_ajustat.m
@@ -2,7 +2,7 @@
 clear;                             
 clf;                              
 
-N = 50; 
+N = 25; 
 P=40;                              
 wo =pi; 
                           
@@ -13,9 +13,9 @@ figure(1)
 
 % SF exponentiala
 
-x = c0*ones(size(t));                     % initializ xce cu c0
+x = c0/2;                     % initializ x cu c0
 
-for n = -N+1:2:N-1,                    
+for n = -N:2:N,                    
   cn = 2/(j*n*wo);                        % Coeficientii seriei fourieri
   x = x + real(cn*exp(j*n*wo*t)); %SFE
 end
@@ -38,11 +38,11 @@ stem(0,c0);
                                    %folosim stem pentru discret
 
 hold;
-for n = -N+1:2:N-1,                    
+for n = -N:2:N,                    
   cn = 2/(j*n*wo);                 % Coeficientii
   stem(n*wo,abs(cn))               
 end
-for n = -N:2:N,                
+for n = -N+1:2:N-1,                
   cn = 0;                         
   stem(n*wo,abs(cn));              
 end
